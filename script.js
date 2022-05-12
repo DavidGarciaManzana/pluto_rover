92cc42f0b412211f658ef10e64373f2f87d465c0
David <Davidgarciama@outlook.com>
2022-05-12 16:36:17 -0500

    Added the option to move below and forward when the rover is looking south

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let y = 0
 let roverPosition = [x, y]
 let roverDirection = "N"
 let F = 1
-let B = -1
+let B = 1
 let L = "Left"
 let R = "Right"
 
@@ -12,22 +12,33 @@ let S = true
 let E = true
 let O = true
 
+//-----------------------------------------MOVE ROVER
 
 // console.log("before moving",roverPosition)
-// let moveRover = (direction, move) => {
-//     if(direction === N) {
-//         if(move === F){
-//             return roverPosition[1] += F
-//         }
-//         if(move === B){
-//             return roverPosition[1] += B
-//         }
-//     }
-//
-//     return roverPosition = []
-// }
+let moveRover = (direction, move) => {
+    if(direction === N) {
+        if(move === F){
+            return roverPosition[1] += F
+        }
+        if(move === B){
+            return roverPosition[1] -= B
+        }
+    }
+    if(direction === S) {
+        if(move === F ){
+            return roverPosition[1] -= F
+        }
+        if(move === B){
+            return roverPosition[1] += B
+        }
+    }
+
+    return roverPosition = []
+}
 // moveRover(N, F)
 // console.log("after moving",roverPosition)
+
+// -------------------------------------------ROTATE ROVER
 
 let rotateRover = (actualDirection = "N", rotateDirection = R) => {
     if (actualDirection === "N") {
@@ -48,13 +59,13 @@ let rotateRover = (actualDirection = "N", rotateDirection = R) => {
     }
     if (actualDirection === "S") {
         if (rotateDirection === R) {
-            return roverDirection = "O"
+            return roverDirection = "W"
         }
         if (rotateDirection === L) {
             return roverDirection = "E"
         }
     }
-    if (actualDirection === "O") {
+    if (actualDirection === "W") {
         if (rotateDirection === R) {
             return roverDirection = "N"
         }
@@ -65,11 +76,13 @@ let rotateRover = (actualDirection = "N", rotateDirection = R) => {
 
 }
 
-rotateRover(roverDirection, R)
-console.log(roverDirection)
-rotateRover(roverDirection, R)
-console.log(roverDirection)
-rotateRover(roverDirection, R)
-console.log(roverDirection)
-rotateRover(roverDirection, R)
-console.log(roverDirection)
+// rotateRover(roverDirection, R)
+// console.log(roverDirection)
+
+rotateRover(roverDirection,R)
+rotateRover(roverDirection,R)
+console.log("dir",roverDirection)
+console.log("pos",roverPosition)
+moveRover(S,B)
+console.log("dir",roverDirection)
+console.log("pos",roverPosition)
