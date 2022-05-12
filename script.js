8e83fcb19c0cd60e351f9e9b1284eb9382f82dd0
David <Davidgarciama@outlook.com>
2022-05-12 16:00:50 -0500

    Added the option to move the rover Forward to the north

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-let roverPosition = [0,0]
-let roverDirection = ["N"]
+let x = 0
+let y = 0
+let roverPosition = [x, y]
+let roverDirection = "N"
+let F = 1
+let B = -1
+let N = true
 
+console.log(roverPosition)
+let moveRover = (direction, move) => {
+    if(N) {
+        if(F){
+            return roverPosition[1] += F
+        }
+    }
+    return roverPosition = []
+}
+moveRover(N, F)
+console.log(roverPosition)
